b3d79800ebf513bb6ec7b0d595a8327ce6219376
Parvinder Singh Gandhi <mr.parvindersinghgandhi@gmail.com>
2026-07-26 15:01:59 +0530

    Adjust Hero component layout for improved spacing; add margin-top to grid container for better responsiveness

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
   return ( 
     <div className="min-h-screen flex items-center pt-20 mx-auto justify-center" id="hero"> 
       <div className="mx-auto min-h-screen max-w-7xl px-6 sm:px-8 lg:px-12"> 
-        <div className="grid items-center gap-16 lg:grid-cols-2"> 
+        <div className="grid items-center gap-16 lg:grid-cols-2 lg:mt-0 mt-14"> 
           <HeroContent /> 
           <HeroSphere className= ""/> 
         </div> 
